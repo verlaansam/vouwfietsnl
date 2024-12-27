da936a9cb0bf281dd915af77cf80f89147ce9d8e
sam <verlaansam@gmail.com>
2024-12-27 10:43:44 +0100

    localstorage hell en prijsen teoveogen

--- src/components/ComponentPicker.js
+++ src/components/ComponentPicker.js
@@ -1,19 +1,23 @@
 //TODO:
-//prijs toevoegen
 //plaatjes toeveogen van comp
+//opslaan item in localstorage https://www.freecodecamp.org/news/how-to-use-localstorage-with-react-hooks-to-set-and-get-items/
 import React from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 import { bikes} from './data';
 import KeuzenHulp from './KeuzenHulp';
 import JouwBrompton from './JouwBrompton';
 
 function ComponentPicker(props) {
+    const [items, setItems] = useState([]);
 
     const BikeType = bikes.filter(bikes =>
         bikes.id === props.model,
-        localStorage.setItem('model', props.model),
-        window.dispatchEvent(new Event('storage'))
+
+        useEffect(() => {
+        localStorage.setItem('BikeT', JSON.stringify(items));
+        }, [items])
     );
 
     function handleClick(e) {
@@ -23,57 +27,106 @@ function ComponentPicker(props) {
     }
 
     const listItemsVersnelling = BikeType.map(bikes =>
-        <div className='flex justify-around' key={bikes.id}>
+        <div className='flex justify-around flex-col' key={bikes.id}>
+            <div className='w-full flex justify-around'>
             {bikes.versnelling.map(versnelling =>
-                <p key={versnelling} id='versnelling' onClick={handleClick}>{versnelling}</p>
+                <button key={versnelling} id='versnelling' onClick={handleClick}>{versnelling}</button>
             )}
+            </div>
+            <div className='w-full flex justify-around'>
+            {bikes.versnellingPrijs.map(versnellingPrijs =>
+                <button key={versnellingPrijs} id='versnellingPrijs' onClick={handleClick}>€{versnellingPrijs}</button>
+            )}
+            </div>
         </div>
     );
 
     const listItemsZadel = BikeType.map(bikes =>
-        <div className='flex justify-around' key={bikes.id}>
+        <div className='flex justify-around flex-col' key={bikes.id}>
+            <div className='w-full flex justify-around'>
             {bikes.zadel.map(zadel =>
-                <p key={zadel}id='zadel' onClick={handleClick}>{zadel}</p>
+                <button key={zadel} id='zadel' onClick={handleClick}>{zadel}</button>
             )}
+            </div>
+            <div className='w-full flex justify-around'>
+            {bikes.zadelPrijs.map(zadelPrijs =>
+                <button key={zadelPrijs} id='zadelPrijs' onClick={handleClick}>€{zadelPrijs}</button>
+            )}
+            </div>
         </div>
     );
 
     const listItemsZadelHoogte = BikeType.map(bikes =>
-        <div className='flex justify-around' key={bikes.id}>
+        <div className='flex justify-around flex-col' key={bikes.id}>
+            <div className='w-full flex justify-around'>
             {bikes.zadelHoogte.map(zadelHoogte =>
-                <p key={zadelHoogte} id='zadelHoogte' onClick={handleClick}>{zadelHoogte}</p>
+                <button key={zadelHoogte} id='zadelHoogtePrijs' onClick={handleClick}>{zadelHoogte}</button>
             )}
+            </div>
+            <div className='w-full flex justify-around'>
+            {bikes.zadelHoogtePrijs.map(zadelHoogtePrijs =>
+                <button key={zadelHoogtePrijs} id='zadelHoogtePrijs' onClick={handleClick}>€{zadelHoogtePrijs}</button>
+            )}
+            </div>
         </div>
     );
 
     const listItemsRack = BikeType.map(bikes =>
-        <div className='flex justify-around' key={bikes.id}>
+        <div className='flex justify-around flex-col' key={bikes.id}>
+            <div className='w-full flex justify-around'>
             {bikes.rack.map(rack =>
-                <p key={rack} id='rack' onClick={handleClick} >{rack}</p>
+                <button key={rack} id='rack' onClick={handleClick}>{rack}</button>
             )}
+            </div>
+            <div className='w-full flex justify-around'>
+            {bikes.rackPrijs.map(rackPrijs =>
+                <button key={rackPrijs} id='versnelling' onClick={handleClick}>€{rackPrijs}</button>
+            )}
+            </div>
         </div>
     );
 
     const listItemsStuur = BikeType.map(bikes =>
-        <div className='flex justify-around' key={bikes.id}>
+        <div className='flex justify-around flex-col' key={bikes.id}>
+            <div className='w-full flex justify-around'>
             {bikes.stuur.map(stuur =>
-                <p key={stuur} id='stuur' onClick={handleClick}>{stuur}</p>
+                <button key={stuur} id='stuur' onClick={handleClick}>{stuur}</button>
             )}
+            </div>
+            <div className='w-full flex justify-around'>
+            {bikes.stuurPrijs.map(stuurPrijs =>
+                <button key={stuurPrijs} id='versnelling' onClick={handleClick}>€{stuurPrijs}</button>
+            )}
+            </div>
         </div>
     );
     const listItemsverlichting = BikeType.map(bikes =>
-        <div className='flex justify-around' key={bikes.id}>
+        <div className='flex justify-around flex-col' key={bikes.id}>
+            <div className='w-full flex justify-around'>
             {bikes.verlichting.map(verlichting =>
-                <p key={verlichting} id='verlichting' onClick={handleClick}>{verlichting}</p>
+                <button key={verlichting} id='verlichting' onClick={handleClick}>{verlichting}</button>
             )}
+            </div>
+            <div className='w-full flex justify-around'>
+            {bikes.verlichtingPrijs.map(verlichtingPrijs =>
+                <button key={verlichtingPrijs} id='verlichtingPrijs' onClick={handleClick}>€{verlichtingPrijs}</button>
+            )}
+            </div>
         </div>
     );
 
     const listItemskleur = BikeType.map(bikes =>
-        <div className='flex justify-around' key={bikes.id}>
+        <div className='flex justify-around flex-col' key={bikes.id}>
+            <div className='w-full flex justify-around'>
             {bikes.kleur.map(kleur =>
-                <p key={kleur} id='kleur' onClick={handleClick}>{kleur}</p>
+                <button key={kleur} id='kleur' onClick={handleClick}>{kleur}</button>
             )}
+            </div>
+            <div className='w-full flex justify-around'>
+            {bikes.kleurPrijs.map(kleurPrijs =>
+                <button key={kleurPrijs} id='kleurPrijs' onClick={handleClick}>€{kleurPrijs}</button>
+            )}
+            </div>
         </div>
     );
 
